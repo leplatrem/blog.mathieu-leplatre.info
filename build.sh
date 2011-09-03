998c38644c5f0941d8bb1d0b2e30944781e50fe3
leplatrem <contact@mathieu-leplatre.info>
2011-09-03 16:53:02 +0200

    Auto deactivate virtualenv

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ source env/bin/activate
 #pip install -U pelican
 rm -rf output
 pelican blog.mathieu-leplatre.info -t pelican-theme -s blog.mathieu-leplatre.info/settings.py
+deactivate
 firefox output/index.html
