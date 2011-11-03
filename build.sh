a8c20cbc2380745659bc231dde812306c0a6f609
Mathieu Leplatre <contact@mathieu-leplatre.info>
2011-11-03 14:57:00 +0100

    New pelican options

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ virtualenv -q env
 source env/bin/activate
 #pip install -U pelican
 rm -rf output
-pelican blog.mathieu-leplatre.info -t pelican-theme -s blog.mathieu-leplatre.info/settings.py
+pelican -v blog.mathieu-leplatre.info -t pelican-theme -s blog.mathieu-leplatre.info/settings.py -o output
 deactivate
 firefox output/index.html
